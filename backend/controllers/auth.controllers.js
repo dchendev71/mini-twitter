45599c353d20f35666fd6e2384a9be4f3daf05ca
user.name <dchen.dev71@gmail.com>
2025-09-14 09:36:36 +0200

    Update: refactoring JS variable into function

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,10 +1,11 @@
 import bcrypt from "bcrypt";
 import users from "../models/user.models.js";
 import jwt from "jsonwebtoken"
+import logger from "../utils/logger.js"
 
 import { generateAccessToken, generateRefreshToken } from "../utils/token.utils.js";
 
-export const registerUser = async (req, res) => {
+async function registerUser(req, res) {
   const { username, password } = req.body;
   if (users.find((u) => u.username === username)) {
     return res.status(400).json({ message: "Username already exists" });
@@ -12,21 +13,24 @@ export const registerUser = async (req, res) => {
   const hashedPassword = await bcrypt.hash(password, 10);
   const newUser = { id: users.length + 1, username, password: hashedPassword };
   users.push(newUser);
-  res.json({ message: "User created" });
-};
 
-export const loginUser = async (req, res) => {
+  logger.info("User created", {username});
+  res.json({ message: "User created" });
+}
+
+async function loginUser(req, res) {
   const { username, password } = req.body;
   const user = users.find((u) => u.username === username);
   if (!user || !(await bcrypt.compare(password, user.password))) {
+    logger.error("Failed login attempt", {username});
     return res.status(401).json({ message: "Invalid credentials" });
   }
   const accessToken = generateAccessToken(user);
   const refreshToken = generateRefreshToken(user);
   res.json({ accessToken, refreshToken });
-};
+}
 
-export const refreshToken = (req, res) => {
+function refreshToken(req, res) {
   const { refreshToken } = req.body;
   if (!refreshToken) return res.sendStatus(401);
   jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, user) => {
@@ -34,4 +38,6 @@ export const refreshToken = (req, res) => {
     const newAccessToken = generateAccessToken(user);
     res.json({ accessToken: newAccessToken });
   });
-};
+}
+
+export { registerUser, loginUser, refreshToken };
