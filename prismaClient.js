c63037839520fd91085e0c17a398fcd41061078d
user.name <dchen.dev71@gmail.com>
2025-09-20 20:20:55 +0200

    Add: add prisma files which set up the DB model

---
+++ prismaClient.js
@@ -0,0 +1,16 @@
+// backend/prismaClient.js
+import { PrismaClient } from "@prisma/client";
+
+let prisma;
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  // Prevent multiple instances during hot reload in dev
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
+export default prisma;
