20587de976bf192c98020a382a8b5c0a7a22096e
user.name <dchen.dev71@gmail.com>
2025-09-20 23:11:03 +0200

    Add: add docker related files and database setup
    add: tests/setupTestDb.js - Init build for the DB
    add: tests/setupTests.js - Init file for test beforeAll()
    add: ../docker-compose.yml - docker compose for fast launch of the DB

---
+++ backend/tests/setupTests.js
@@ -0,0 +1,9 @@
+import { setupTestDb } from "./setupTestDb.js";
+import dotenv from "dotenv"
+
+dotenv.config()
+
+beforeAll(async () => {
+  process.env.DATABASE_URL = process.env.DATABASE_URL_TEST; // override DB URL
+  await setupTestDb(); // reset DB before all tests
+});
