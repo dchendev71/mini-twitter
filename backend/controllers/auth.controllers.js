d4864c3263d2b82e86a0be0499a84f7bc9714b87
user.name <dchen.dev71@gmail.com>
2025-09-23 20:54:11 +0200

    Update: Added tests for coverage
    fixed linting on all files

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,23 +1,26 @@
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import logger from "../utils/logger.js";
-import prisma from "../prismaClient.js"
+import prisma from "../prismaClient.js";
 
 import {
   generateAccessToken,
   generateRefreshToken,
 } from "../utils/token.utils.js";
 
-
 async function registerUser(req, res) {
   const { username, password, email } = req.body;
- 
+
   if (!username || !password || !email) {
-    return res.status(400).json({ message: "username, password and email required"});
+    return res
+      .status(400)
+      .json({ message: "username, password and email required" });
   }
 
   if (await checkIfUserExists(username, email)) {
-    return res.status(400).json({ message: "Username or email already in use"});
+    return res
+      .status(400)
+      .json({ message: "username or email already in use" });
   }
 
   const hashedPassword = await bcrypt.hash(password, 10);
@@ -25,39 +28,39 @@ async function registerUser(req, res) {
     data: {
       username,
       email,
-      password: hashedPassword
-    }
+      password: hashedPassword,
+    },
   });
 
-  logger.info("User created", { username });
+  logger.info("user created", { username });
 
   return res.json({
-    message: "User created",
-    user: { id: user.id, username: user.username, email: user.email }
-  })
+    message: "user created",
+    user: { id: user.id, username: user.username, email: user.email },
+  });
 }
 
 async function checkIfUserExists(username, email) {
-  if (await prisma.user.findUnique({where: { username } })) {
+  if (await prisma.user.findUnique({ where: { username } })) {
     return true;
   }
-  if (await prisma.user.findUnique({where: { email } })) {
+  if (await prisma.user.findUnique({ where: { email } })) {
     return true;
   }
 
-  return false
+  return false;
 }
 
 async function loginUser(req, res) {
   const { username, password } = req.body;
   if (!username || !password) {
-    return res.status(400).json({ message: "username and password required"});
+    return res.status(400).json({ message: "username and password required" });
   }
-  const user = await prisma.user.findUnique({ where: { username }})
-  if (!user|| !await bcrypt.compare(password, user.password)) {
+  const user = await prisma.user.findUnique({ where: { username } });
+  if (!user || !(await bcrypt.compare(password, user.password))) {
     // We do not want to tell them that the user does not exist
-    logger.error("Failed login attempt", { username });
-    return res.status(401).json({ message: "Invalid credentials"})
+    logger.error("failed login attempt", { username });
+    return res.status(401).json({ message: "invalid credentials" });
   }
   const accessToken = generateAccessToken(user);
   const refreshToken = generateRefreshToken(user);
@@ -69,7 +72,10 @@ function refreshToken(req, res) {
   if (!refreshToken) return res.sendStatus(401);
   jwt.verify(refreshToken, process.env.JWT_REFRESH_SECRET, (err, payload) => {
     if (err) return res.sendStatus(403);
-    const newAccessToken = generateAccessToken({ id: payload.id, username: payload.username });
+    const newAccessToken = generateAccessToken({
+      id: payload.id,
+      username: payload.username,
+    });
     return res.json({ accessToken: newAccessToken });
   });
 }
--- backend/tests/auth.test.js
+++ backend/tests/auth.test.js
@@ -2,18 +2,18 @@ import request from "supertest";
 import app from "../server.js"; // export app from server.js
 
 const USERNAME = "testuser";
-const PASSWORD = "testpwd"
-const EMAIL = "testuser@test.com"
+const PASSWORD = "testpwd";
+const EMAIL = "testuser@test.com";
 
 const bodyWithEmail = {
   username: USERNAME,
   password: PASSWORD,
-  email: EMAIL
+  email: EMAIL,
 };
 
 const body = {
   username: USERNAME,
-  password: PASSWORD
+  password: PASSWORD,
 };
 
 async function getResponse(route, body) {
@@ -21,17 +21,69 @@ async function getResponse(route, body) {
 }
 
 describe("Auth Routes", () => {
+  it("should fail registering user, missing email", async () => {
+    const res = await getResponse("/auth/register", body);
+
+    expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty(
+      "message",
+      "username, password and email required",
+    );
+  });
+
   it("should register a user", async () => {
     const res = await getResponse("/auth/register", bodyWithEmail);
 
     expect(res.statusCode).toBe(200);
-    expect(res.body).toHaveProperty("message", "User created");
+    expect(res.body).toHaveProperty("message", "user created");
   });
 
   it("should not register same username twice", async () => {
     const res = await getResponse("/auth/register", bodyWithEmail);
 
     expect(res.statusCode).toBe(400);
+    expect(res.body).toHaveProperty(
+      "message",
+      "username or email already in use",
+    );
+  });
+
+  it("should not register same email twice, different username", async () => {
+    const payload = {
+      username: "TEST",
+      password: PASSWORD,
+      email: EMAIL,
+    };
+
+    const res = await getResponse("/auth/register", payload);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty(
+      "message",
+      "username or email already in use",
+    );
+  });
+
+  it("should fail login, missing password", async () => {
+    const payload = { username: USERNAME };
+    const res = await getResponse("/auth/login", payload);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty(
+      "message",
+      "username and password required",
+    );
+  });
+
+  it("should fail login, missing username", async () => {
+    const payload = { password: PASSWORD };
+    const res = await getResponse("/auth/login", payload);
+
+    expect(res.status).toBe(400);
+    expect(res.body).toHaveProperty(
+      "message",
+      "username and password required",
+    );
   });
 
   it("should login succesfully", async () => {
@@ -52,7 +104,7 @@ describe("Auth Routes", () => {
       password: PASSWORD + "123",
     });
     expect(res.statusCode).toBe(401);
-    expect(res.body).toHaveProperty("message", "Invalid credentials");
+    expect(res.body).toHaveProperty("message", "invalid credentials");
   });
 
   it("should issue a new access token with a valid refresh token", async () => {
