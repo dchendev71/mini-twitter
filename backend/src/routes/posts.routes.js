46f95c40e671015deeb59f3df0685ee16b3d298a
user.name <dchen.dev71@gmail.com>
2025-10-02 22:54:27 +0200

    add: Add fanoutqueue, worker and redis integration

--- backend/src/routes/posts.routes.js
+++ backend/src/routes/posts.routes.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getTimeline } from "../controllers/post.controller.js";
 import { authenticateToken } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/", authenticateToken, createPost);
+router.post("/timeline", authenticateToken, getTimeline);
 
 export default router;
